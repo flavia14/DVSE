2ba3a861ad795425c22b45e2fe00424b7cd36d07
flavia14 <91269712+flavia14@users.noreply.github.com>
2022-06-15 10:47:31 +0300

    Add files via upload

---
+++ DVSE/js/proiecte/proiect1/index.js
@@ -0,0 +1,54 @@
+console.log("TEMA 1")
+let count = 222222
+console.log(count)
+
+
+console.log("TEMA 2")
+let myAge =22
+console.log(myAge)
+let humanDogRatio =2
+let myDogage=humanDogRatio*myAge
+console.log("In ani de caine as avea varsta")
+console.log(myDogage)
+
+console.log("TEMA 3")
+let bonusPoints=50
+console.log(bonusPoints)
+bonusPoints=bonusPoints+100
+console.log(bonusPoints)
+bonusPoints=bonusPoints-25
+console.log(bonusPoints)
+bonusPoints=bonusPoints+70
+console.log(bonusPoints)
+
+
+console.log("TEMA 4")
+function increment()
+{
+
+    console.log("A fost apasat butonul!")
+}
+
+
+console.log("TEMA 5")
+console.log("Functia mea")
+function Flavia()
+{
+ console.log(42);
+}
+
+Flavia();
+
+console.log("TEMA 6")
+let lap1=2
+let lap2=3
+let lap3=4
+function suma()
+{
+    let sum= lap1+lap2+lap3
+    console.log("Suma este " + sum)
+}
+
+suma()
+
+
